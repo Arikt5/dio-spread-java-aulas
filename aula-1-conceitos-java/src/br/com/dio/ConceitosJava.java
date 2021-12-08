0d206a3a8dbb96777b72464eeb1d3daaf8b181bd
Arikt5 <mary.vitoriahp@gmail.com>
2021-12-08 19:51:13 -0300

    nomes-variaveis

---
+++ aula-1-conceitos-java/src/br/com/dio/ConceitosJava.java
@@ -0,0 +1,51 @@
+package br.com.dio;
+
+public class ConceitosJava {
+
+    public static void main(String[] args) {
+
+        //praticando nomes de variásveis;
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn24678md;
+        //int asrn 24678md;
+        int asrn2$4678_md;
+        //int asrn2$46%78_md =10;
+
+        asrn24678md = 100;
+        asrn2$4678_md = 10;
+
+        int quantidadedeProduto = 50;
+        //int QuntidadedeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn2$4678_md);
+
+        System.out.println(quantidadedeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+    }
+}
